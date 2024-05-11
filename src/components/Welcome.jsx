7f986881f2844e2334f80ef5e157014861ffb0d3
Smi-lin <olajideolabimpe26@gmail.com>
2024-05-11 14:10:38 +0100

    first commit

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,6 +5,17 @@ import { BsInfoCircle } from "react-icons/bs";
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white";
 
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    stop="0.001"
+    value={value}
+    // onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
+
 const Welcome = () => {
   return (
     <div className="flex w-full justify-center items-center">
@@ -49,9 +60,13 @@ const Welcome = () => {
             </div>
           </div>
 
-
           <div>
-            
+            <Input
+              placeholder="Wallet Address"
+              name="Address To"
+              type="Text"
+              // handleChange={handleChange}
+            />
           </div>
         </div>
       </div>
