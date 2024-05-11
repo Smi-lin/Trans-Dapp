be686af8af60d8c2fc544ab7e6bb0ecab2471eeb
Smi-lin <olajideolabimpe26@gmail.com>
2024-05-11 13:52:03 +0100

    first commit

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import {SiEthereum} from "react-icons/si";
-import {BsInfoCircle} from "react-icons/bs"
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
 
-const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white"
+const commonStyles =
+  "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-white";
 
 const Welcome = () => {
   return (
@@ -17,31 +18,34 @@ const Welcome = () => {
           </p>
           <button className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
             <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button> 
+          </button>
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${commonStyles}`}>Relaibility</div>
             <div className={` ${commonStyles}`}>Security</div>
-            <div className={`rounded-tl-2xl ${commonStyles}`}>Ethereum</div>
-            <div className={`rounded-tl-2xl ${commonStyles}`}>Easy Send</div>
+            <div className={`rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
+            <div className={`rounded-bl-2xl ${commonStyles}`}>Easy Send</div>
             <div className={` ${commonStyles}`}>Low Fess</div>
-            <div className={`rounded-tl-2xl ${commonStyles}`}>Blockchain</div>
+            <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
           </div>
         </div>
 
-
-        <div>
-          <div className="flex flex-col flex-1 items-start justify-start w-full mf:mt-0 mt-10">
-            <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
-              <div className="flex justify-between flex-col w-full h-full">
-               <div className="flex justify-between items-start">
-               <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                  <SiEthereum fontSize={21} color="#fff"/>
+        <div className="flex flex-col flex-1 items-start justify-start w-full mf:mt-0 mt-10">
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
                 </div>
-                <BsInfoCircle fontSize={17} color="#fff"/>
-               </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
               </div>
-
-              <p>0x6Db6.........1F6a9</p>
+              <div>
+                <p className="text-black font-bold text-lg">
+                  0x6Db6.........1F6a9
+                </p>
+                <p className="text-white font-semibold text-lg mt-1 ">
+                  Ethereum
+                </p>
+              </div>
             </div>
           </div>
         </div>
